82b35cb9501cd9e4da483320993ce8f756ee6bc0
Abbi H Safaie <Abbi@Abbis-MacBook-Air.local>
2014-03-27 00:51:50 +1000

    Initial Commit

---
+++ Milstone4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Milstone4
+//
+//  Created by Abbi H Safaie on 27/03/2014.
+//  Copyright (c) 2014 Abbas H Safaie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Milstone4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Milstone4
+//
+//  Created by Abbi H Safaie on 27/03/2014.
+//  Copyright (c) 2014 Abbas H Safaie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
